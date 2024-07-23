fcbd9589a55c91d2c829999f609f92b985cbe744
Nikita Voronkov <ntVoronkov@yandex.ru>
2024-07-23 13:32:23 +0300

    Refactor signupview in api/auth_views.py. Refactor CustomUser serializers in api/serializers.py.

--- api_yamdb/api/error_constants.py
+++ api_yamdb/api/error_constants.py
@@ -1,6 +1,7 @@
 INVALID_USERNAME: str = ('Username can only contain letters,'
                          'numbers, and @/./+/-/_ characters.')
 INVALID_EMAIL: str = 'User already has another email.'
+EMAIL_MISMATCH: str = 'This email already has another user.'
 INVALID_CODE: str = 'Invalid confirmation code.'
 ME_NOT_ALLOWED: str = "Username 'me' is not allowed."
 USER_NOT_FOUND: str = 'User not found.'
